c9dd60effa5c343c8bd228648391f1288c37ed4c
boba2fett <boba2002fett@gmail.com>
2020-06-06 23:57:35 +0200

    basic struckture and colors

--- b2fsh.py
+++ b2fsh.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import sys, os
 from cmd import Cmd
+from colorama import Fore, Back, Style
 
+
+#Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+#Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+#Style: DIM, NORMAL, BRIGHT, RESET_ALL
 
 target=""
+fork=False
 
 
 class MyPrompt(Cmd):
@@ -18,16 +24,54 @@ class MyPrompt(Cmd):
         pass
         
     def do_exit(self, inp):
+        global fork
+        if fork:
+            forkbomb()
         return True
  
 
+def gather_infos():
+    log("getting most important information:")
+    global target
+    pass
 
-def main(target, auto: ('extracts the most important information', 'flag', 'a'),full: ('extracts the all important information', 'flag', 'f')):
+def gather_more():
+    global target
+    gather_infos()
+    log("getting more information:")
+    pass
+
+def download(path):
+    global target
+    pass
+
+def upload(path):
+    global target
+    pass
+
+def forkbomb():
+    log("leaving forkbomb")
+    pass
+
+def log(msg):
+    print(Fore.CYAN+msg+Style.RESET_ALL)
+
+def warn(msg):
+    print(Fore.YELLOW+msg+Style.RESET_ALL)
+
+def main(target, infos: ('extracts the most important information', 'flag', 'i'),more: ('extracts the all important information', 'flag', 'm'),fork: ('leaves forkbomb', 'flag', 'f'),auto: ('non interactive', 'flag', 'a')):
     "Use with b2fshell.php or b2fshell-headless.php"
     globals()['target'] = target
-    globals()['auto'] = auto
-    globals()['full'] = full
-    MyPrompt().cmdloop()
+    globals()['fork'] = fork
 
+    if more:
+        gather_more()
+    elif infos:
+        gather_infos()
+
+    if not auto:
+        MyPrompt().cmdloop()
+    else:
+        warn("--auto specified: terminating")
 if __name__ == '__main__':
     import plac; plac.call(main)
